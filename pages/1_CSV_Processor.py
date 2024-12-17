a7df4104a6c45bd31ee1d7d13d59569ab5a7254a
Guangmou Deng <guangmou01@gmail.com>
2024-12-17 12:48:52 +0000

    Update 1_CSV_Processor.py

--- pages/1_CSV_Processor.py
+++ pages/1_CSV_Processor.py
@@ -13,40 +13,44 @@ st.markdown("---")
 
 
 st.markdown("### Please Upload a .csv File:")
-uploaded_file = st.file_uploader("Upload a target-file", type=["csv"])
+uploaded_file = st.file_uploader("Upload a target file", type=["csv"])
 
 if uploaded_file is not None:
-    st.markdown("### Does It Have a Header?")
-    header_option = st.radio("Choose whether to remove the header", options=["Yes", "No"])
+    st.markdown("### Pre-process:")
+    header_option = st.selectbox("Choose whether to remove the header:", options=["Yes", "No"])
+
 
     if header_option == "Yes":
         df = pd.read_csv(uploaded_file)
     else:
         df = pd.read_csv(uploaded_file, header=None)
 
-    st.markdown("### Dataframe:")
     st.dataframe(df)
 
     if not df.empty:
-        col_options = df.columns if header_option == "Yes" else [f"Column {i+1}" for i in range(df.shape[1])]
-        selected_col = st.selectbox("Which column do you want to extract", options=col_options)
+        col1, col2 = st.columns(2)
 
-        if header_option == "Yes":
-            column_data = df[selected_col].dropna()
-        else:
-            col_index = int(selected_col.split(" ")[1]) - 1
-            column_data = df.iloc[:, col_index].dropna()
+        with col1:
+            col_options = df.columns if header_option == "Yes" else [f"Column {i + 1}" for i in range(df.shape[1])]
+            selected_col = st.selectbox("Which column do you want to extract:", options=col_options)
 
-        st.markdown("### Transformation Options:")
-        transformation = st.radio("Choose a transformation",
-                                  options=["➡️ Raw", "➡️ Base-10 Logarithm", "➡️ Power of 10"])
+            if header_option == "Yes":
+                column_data = df[selected_col].dropna()
+            else:
+                col_index = int(selected_col.split(" ")[1]) - 1
+                column_data = df.iloc[:, col_index].dropna()
 
+        with col2:
+            transformation = st.selectbox("Choose a transformation:",
+                                          options=["Raw data", "Base-10 Logarithm", "Power of 10"])
+
+        vector_data = ""
         try:
-            if transformation == "➡️ Base-10 Logarithm":
+            if transformation == "Base-10 Logarithm":
                 column_data = column_data.astype(float)
                 transformed_data = np.log10(column_data.replace(0, np.nan)).dropna()
                 st.warning("⚠️ Values <= 0 are removed during Log10 transformation.")
-            elif transformation == "➡️ Power of 10":
+            elif transformation == "Power of 10":
                 column_data = column_data.astype(float)
                 transformed_data = 10 ** column_data
             else:
@@ -54,7 +58,7 @@ if uploaded_file is not None:
 
             vector_data = ",".join(transformed_data.astype(str))
 
-            st.markdown("### Preview:")
+            st.markdown("### Pre-view:")
             st.code(vector_data)
 
             buffer = io.BytesIO()
@@ -67,5 +71,6 @@ if uploaded_file is not None:
                 file_name=f"{selected_col}.txt",
                 mime="text/plain"
             )
+
         except ValueError:
             st.error("❌ Error: Ensure the selected column contains only numeric values for transformation.")
